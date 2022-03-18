7ecae878facbd71483bb57ef02cbed8507369d1f
ageu_dev19 <ageumenezes423@gmail.com>
2022-03-18 11:59:52 -0300

    Change my projects style using the transform and install npm

--- script.js
+++
@@ -1,8 +0,0 @@
-const countProjects = () => {
-    const myProjectsNumbers = document.querySelectorAll('.projects-container').length;
-    const pOfTheNumbers = document.querySelector('#numbers-of-projects p');
-    const outProjectsNumb = 1;
-    pOfTheNumbers.innerText = `${myProjectsNumbers + outProjectsNumb} Projects`;
-}
-
-countProjects();
